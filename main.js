7a1bba80b2a1fb58a4c2dfa4dfdacfa8d9b8d9e6
Alexander3006 <ukrpresident2036@gmail.com>
2020-10-14 22:23:20 +0300

    add: register HttpTransport

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const ApplicationBuilder = require('./Framework/ApplicationBuilder');
+const configurations = require('./config');
+const HttpTransport = require('./Framework/Transport/http/HttpTransport');
+const HttpConnection = require('./Framework/Transport/http/HttpConnection');
 
 (async () => {
-  const applicationBuilder = new ApplicationBuilder();
+  const applicationBuilder = new ApplicationBuilder()
+    .setConfigurations(configurations)
+    .useTransport(HttpTransport, HttpConnection);
   await applicationBuilder.buildServices();
   await applicationBuilder.build();
 })();
