8746a901448b552d7e035252a4bdae61c02a71db
Alexander3006 <ukrpresident2036@gmail.com>
2020-10-16 22:44:19 +0300

    add: example controller

---
+++ Controllers/TestController.js
@@ -0,0 +1,11 @@
+const testController = async (connection, services) => {
+    const {logger} = services;
+    logger.print("Test controller");
+    connection.sendJson('Hello from testController')
+}
+
+({
+    url: '/test',
+    method: 'GET',
+    handler: testController,
+})
