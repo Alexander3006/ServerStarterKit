f1b485787fe44b2bd94a6fdae72f77ec9cff3f01
Alexander3006 <ukrpresident2036@gmail.com>
2020-10-16 22:41:08 +0300

    change: set dependencies

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const ApplicationBuilder = require('./Framework/ApplicationBuilder');
-const configurations = require('./config');
+const configuration = require('./config');
+const dependencies = require(configuration.dependencies);
 const HttpTransport = require('./Framework/Transport/http/HttpTransport');
 const HttpConnection = require('./Framework/Transport/http/HttpConnection');
 
 (async () => {
   const applicationBuilder = new ApplicationBuilder()
-    .setConfigurations(configurations)
+    .setConfigurations(configuration)
+    .setDependencies(dependencies)
     .useTransport(HttpTransport, HttpConnection);
   await applicationBuilder.buildServices();
   await applicationBuilder.build();
