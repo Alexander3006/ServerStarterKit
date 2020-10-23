f15372eac54a9c823258e723d9ed966ea21b7112
Alexander3006 <ukrpresident2036@gmail.com>
2020-10-23 17:42:59 +0300

    add: Sessions

---
+++ Framework/Services/Sessions/SessionService.js
@@ -0,0 +1,59 @@
+class Sessions {
+  constructor(sessionStorage) {
+    const TOKEN_LEN = 256 / 8;
+    this.sessionStorage = sessionStorage;
+    this.configuration = configuration.sessions;
+    this._tokenLength = TOKEN_LEN;
+  }
+
+  async _generateToken() {
+    const {crypto} = npm;
+    const {_tokenLength} = this;
+    const token = await new Promise((res, rej) => {
+      crypto.randomBytes(_tokenLength, (err, buf) => {
+        err ? rej(err) : res(buf);
+      });
+    }).then((x) => x.toString('hex'));
+    return token;
+  }
+
+  async create(connection, data) {
+    const {sessionStorage, configuration} = this;
+    const {maxAge, httpOnly, path} = configuration;
+    const token = await this._generateToken();
+    await sessionStorage.set(token, data);
+    connection.setCookie('token', token, {
+      maxAge,
+      httpOnly,
+      path,
+    });
+    return token;
+  }
+
+  async restore(connection) {
+    const {sessionStorage} = this;
+    const cookie = connection.getCookies();
+    const {token} = cookie;
+    if (token) {
+      const session = await sessionStorage.get(token);
+      return session;
+    }
+    return;
+  }
+
+  async delete(connection, token) {
+    const {sessionStorage, cookieConfiguration} = this;
+    const {httpOnly, path} = cookieConfiguration;
+    await sessionStorage.delete(token);
+    connection.setCookie('token', token, {
+      maxAge: 0,
+      httpOnly,
+      path,
+    });
+  }
+}
+
+SessionsProvider = (sessionStorage) => {
+  const sessions = new Sessions(sessionStorage);
+  return sessions;
+};
---
+++ Framework/Services/Sessions/SessionStorage.js
@@ -0,0 +1,39 @@
+class SessionStorage {
+  constructor(memoryCache) {
+    this.storage = memoryCache;
+  }
+
+  set(token, data) {
+    const {storage} = this;
+    const json = JSON.stringify(data);
+    return new Promise((res, rej) => {
+      storage.set(token, json, (err, reply) => {
+        err ? rej(err) : res(reply);
+      });
+    });
+  }
+
+  async get(token) {
+    const {storage} = this;
+    const session = await new Promise((res, rej) => {
+      storage.get(token, (err, reply) => {
+        err ? rej(err) : res(reply);
+      });
+    });
+    return JSON.parse(session);
+  }
+
+  delete(token) {
+    const {storage} = this;
+    return new Promise((res, rej) => {
+      storage.del(token, (err, reply) => {
+        err ? rej(err) : res(reply);
+      });
+    });
+  }
+}
+
+SessionStorageProvider = (memoryCache) => {
+  const sessionStorage = new SessionStorage(memoryCache);
+  return sessionStorage;
+};
