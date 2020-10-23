3ecf7c84bb49ef43efcfba54e9dc5a5a1beb8abc
Alexander3006 <ukrpresident2036@gmail.com>
2020-10-23 17:42:28 +0300

    add: memoryCache (redis)

---
+++ Framework/Infrastructure/MemoryCache.js
@@ -0,0 +1,19 @@
+class MemoryCache {
+  constructor(logger) {
+    const {redis} = npm;
+    const {memoryCache} = configuration;
+    this.client = redis.createClient(memoryCache);
+    this.client.on('connect', () => {
+      logger.print(`Memory cache connected`);
+    });
+    this.client.on('error', (err) => {
+      logger.print(err);
+    });
+    return this.client;
+  }
+}
+
+MemoryCacheProvider = (logger) => {
+  const memoryCache = new MemoryCache(logger);
+  return memoryCache;
+};
