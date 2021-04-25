fd8d34002b679209ac5c012cfe66204ed79f2d9f
Alexander3006 <ukrpresident2036@gmail.com>
2021-04-25 18:50:56 +0300

    add: transport

--- Framework/ApplicationBuilder.js
+++ Framework/ApplicationBuilder.js
@@ -3,6 +3,7 @@
 const DIContainer = require('./DI/DIContainer');
 const vm = require('vm');
 const fsp = require('fs').promises;
+const ControllerService = require('./Services/Controllers/ControllerService');
 
 class ApplicationBuilder {
   constructor() {
@@ -12,27 +13,34 @@ class ApplicationBuilder {
     this.startup;
     this.configuration = {};
     this.servicesPaths;
+    this.application;
   }
 
   setConfigurations(configuration) {
-    this.configuration = configuration;
+    this.configuration = Object.freeze(configuration);
     return this;
   }
 
   setDependencies(dependencies) {
-    const { npm, nodeApi, services } = dependencies;
-    this.dependencies = { npm, nodeApi };
-    this.servicesPaths = services;
+    const {node_modules, nodeApi, services, adapters, ports, routers} = dependencies;
+    this.dependencies = Object.freeze({
+      node_modules,
+      nodeApi,
+      adapters,
+      routers,
+      ports,
+    });
+    this.servicesPaths = {...services, ...routers};
     return this;
   }
 
   async buildServices() {
-    const { servicesPaths, dependencies, configuration } = this;
+    const {servicesPaths, dependencies, configuration} = this;
     await Promise.all(
       Object.keys(servicesPaths).map(async (serviceName) => {
         const servicePath = servicesPaths[serviceName];
         const src = await fsp.readFile(servicePath);
-        const sandbox = vm.createContext({ ...dependencies, configuration });
+        const sandbox = vm.createContext({...dependencies, ...configuration});
         const script = vm.createScript(src);
         const service = script.runInNewContext(sandbox);
         this.services[serviceName] = service;
@@ -41,17 +49,39 @@ class ApplicationBuilder {
   }
 
   async build() {
-    const { services, dependencies, configuration } = this;
-    const context = { ...services, ...dependencies, configuration };
+    const {services, dependencies, configuration} = this;
+    const context = {...services, ...dependencies, configuration};
     const sandbox = vm.createContext(context);
     const src = await fsp.readFile(configuration.startup);
     const script = vm.createScript(src);
     const Startup = script.runInNewContext(sandbox);
     this.startup = new Startup();
-    const { startup, container } = this;
+    const {startup, container} = this;
+    const {routers} = dependencies;
+    Object.keys(routers).map((routerName) => {
+      container.addSingleton(routerName, services[routerName]);
+    });
     startup.configureServices(container);
     const application = container.build();
+    this.application = application;
     await startup.configure(application);
+    return this;
+  }
+
+  useTransport(Transport, Endpoint, configuration) {
+    const {application} = this;
+    const {logger} = application;
+    const transport = new Transport(application, configuration.transport);
+    const router = application[configuration.router];
+    const {controllers} = configuration;
+    if (controllers) {
+      const controllerService = new ControllerService(router, logger, Endpoint);
+      controllerService.start(controllers);
+    }
+    transport.setHandler(async (request) => {
+      await router.indicatePath(request);
+    });
+    return transport;
   }
 }
 
